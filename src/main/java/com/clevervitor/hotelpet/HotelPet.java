1a7faa029fb157d0dd68d67623b8d5a20836d494
Clever <clever.filho@gmail.com>
2023-11-23 20:59:23 -0300

    Ajuste  Postgre3

--- src/main/java/com/clevervitor/hotelpet/HotelPet.java
+++ src/main/java/com/clevervitor/hotelpet/HotelPet.java
@@ -3,14 +3,14 @@
  */
 package com.clevervitor.hotelpet;
 
-
 import com.clevervitor.hotelpet.controller.FuncionarioController;
 import com.clevervitor.hotelpet.model.entities.Funcionario;
 import com.clevervitor.hotelpet.valid.ValidateUtils;
 import com.clevervitor.hotelpet.view.dialogs.DlgCadPet;
 import com.clevervitor.hotelpet.view.FrLogin;
 import com.clevervitor.hotelpet.view.FrMainMenuClient;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,15 +19,14 @@ import com.clevervitor.hotelpet.view.FrMainMenuClient;
 public class HotelPet {
 
     public static void main(String[] args) {
-                ValidateUtils vLogin = new ValidateUtils();
-        boolean b;
-
-        
-//         FuncionarioController fC = new FuncionarioController();
+//        ValidateUtils vLogin = new ValidateUtils();
+//        boolean b = false;
+//
+//        FuncionarioController fC = new FuncionarioController();
 //        Funcionario f = new Funcionario();
-//        
+//
 //        //Create Func Admin
-//       f.setCargo("Admin");
+//        f.setCargo("Admin");
 //        f.setCpf("11111111111");
 //        f.setDataNasc("01/01/2023");
 //        f.setEmail("admin@admin.com");
@@ -39,14 +38,18 @@ public class HotelPet {
 //        f.setSexo("Masculino");
 //        f.setTel("3299999999");
 //        f.setTurno("Diurno");
-//        
+//
 //        fC.cadastrarFuncionario(f);
 //
-//        b = vLogin.validateLogin("admin@admin.com", "admin");
+//        try {
+//            b = vLogin.validateLogin("admin@admin.com", "admin");
+//        } catch (Exception ex) {
+//            Logger.getLogger(HotelPet.class.getName()).log(Level.SEVERE, null, ex);
+//        }
 //        System.err.println(b);
-            FrLogin TLogin = new FrLogin();
-            TLogin.setTitle("Login");
-            TLogin.setVisible(true);
- 
+        FrLogin TLogin = new FrLogin();
+        TLogin.setTitle("Login");
+        TLogin.setVisible(true);
+
     }
 }
